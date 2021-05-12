fe516a63cdd97de234ad490728092136557f7cc6
Wojciech Maziarz <Wojciech255Gmail.com>
2021-05-12 16:39:47 +0200

    19.2. Tworzenie połączenia z bazą danych z poziomu aplikacji

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,19 +1,29 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
 
-    @GetMapping(value = "getTasks")
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
+
 
     @GetMapping(value = "getTask")
     public TaskDto getTask(@RequestParam Long taskId) {
@@ -23,6 +33,7 @@ public class TaskController {
     @DeleteMapping(value = "deleteTask")
     public void deleteTask(@RequestParam Long taskId) {
 
+        service.deleteTask(taskId);
     }
 
     @PutMapping(value = "updateTask")
@@ -30,8 +41,9 @@ public class TaskController {
         return new TaskDto(1L, "Edited test title", "Test content");
     }
 
-    @PostMapping(value = "createTask")
+    @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createTask(@RequestBody TaskDto taskDto) {
-
+        Task task = taskMapper.mapToTask(taskDto);
+        service.saveTask(task);
     }
 }
