8952ea2f8aaff443f5d3e31259aa9d5b78e963d0
Wojciech Maziarz <Wojciech255Gmail.com>
2021-05-19 08:16:31 +0200

    Fix

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/task")
 @RequiredArgsConstructor
