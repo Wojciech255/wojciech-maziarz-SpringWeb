7f2af507a32813f043c160ee980460a3b3e4b0a4
Wojciech Maziarz <Wojciech255Gmail.com>
2021-07-12 12:08:42 +0200

    17.3-17.4

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,12 @@
+package com.crud.tasks.domain;
+
+import lombok.Data;
+
+@Data
+//@Getter - @ data zawiera te wszystkie adnotacje
+//@AllArgsConstructor
+//@NoArgsConstructor
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+}
